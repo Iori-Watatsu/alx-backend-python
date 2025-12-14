8dea02b86488efd6f8bd448de9a170d2a80032ad
Kamohelo <kamohelotshabalala@proton.me>
2025-12-15 00:05:16 +0200

    Add and modify code in chats/views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,15 +4,26 @@ from .models import Message, Conversation
 from .serializers import MessageSerializer, ConversationSerializer
 from .permissions import IsOwner
 from .permissions import IsParticipantOfConversation
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import IsParticipantOfConversation
+from .filters import MessageFilter
+from .pagination import MessagePagination
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsParticipantOfConversation]
+    pagination_class = MessagePagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
 
     def get_queryset(self):
         # Ensure users can only see their own messages
-        return Message.objects.filter(user=self.request.user)
+        return Message.objects.filter(
+            conversation__participants=self.request.user
+        ).order_by('-created_at')
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
